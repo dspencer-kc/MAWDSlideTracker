222224247304003539fd2ab246ed67b37ac36901
Drew <drw.spencer@gmail.com>
2019-02-01 16:32:44 -0600

    Adding all files

---
+++ vuefrontend/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import BootstrapVue from "bootstrap-vue"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(VueAxios, axios);
+
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
