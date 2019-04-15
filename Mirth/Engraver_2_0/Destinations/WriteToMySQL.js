e33904df493f06affe7273667d87d1808949f41a
Drew <drw.spencer@gmail.com>
2019-04-15 11:06:08 -0500

    Pulled from Live 04152019

--- Mirth/Engraver_2_0/Destinations/WriteToMySQL.js
+++ Mirth/Engraver_2_0/Destinations/WriteToMySQL.js
@@ -25,90 +25,99 @@ strSpecimenID = channelMap.get("strSpecimenID");
 try {
   dbConnMYSQL = DatabaseConnectionFactory.createDatabaseConnection(strMYSQLJDBCDriver, strMYSQLJDBCConnection,strMYSQLUserName,strMYSQLPassword);
 
-	strSQL = "INSERT INTO `tblBlock` \
-			(`BlockID`, \
-			`Hopper`, \
-			`WorkstationID`, \
-			`SpecNumFormatted`, \
-			`PatientName`, \
-			`SpecimenYear`, \
-			`SpecimenNumber`, \
-			`Template`, \
-			`Specimen_id`, \
-			`DateTimeEngraved`, \
-			`PartInst`, \
-			`BlockInst`, \
-			`WhoEngraved`, \
-			`PartType`, \
-			`PartDescription`, \
-			`DateOfService`, \
-			`DateReceived`, \
-			`Protocol`, \
-			`Sequence`, \
-			`PartSequence`, \
-			`PartComment`, \
-			`Pieces`, \
-			`BlockDept`, \
-			`RequestClass`, \
-			`BlockDesignator`, \
-			`PartDesignator`, \
-			`BlockComment`) \
-		VALUES \
-			('" + $('strBlockID') + "', \
-			'" + $('strHopper') + "', \
-			'" + $('strWorkstationID') + "', \
-			'" + $('strSpecNumFormatted') + "', \
-			'" + $('strPatientName') + "', \
-			'" + $('strSpecYear') + "', \
-			'" + $('strSpecNum') + "', \
-			'" + $('strTemplate') + "', \
-			'" + strSpecimenID + "', \
-			'" + strDateTime + "', \
-			'" + $('intPartInst') + "', \
-			'" + $('intBlockInst') + "', \
-			'" + $('strWhoEngraved') + "', \
-			'" + $('strPartType') + "', \
-			'" + $('strPartDesc') + "', \
-			'" + $('strDateTaken') + "', \
-			'" + $('strDateReceived') + "', \
-			'" + $('strProtocolID') + "', \
-			'" + $('intBlockSequence') + "', \
-			'" + $('intPartSequence') + "', \
-			'" + $('strPartComment')  + "', \
-			'" + $('intBlockPieces') + "', \
-			'" + $('strBlockDept') + "', \
-			'" + $('strRequestClass') + "', \
-			'" + $('strBlockLabel') + "', \
-			'" + $('strSpecLabel') + "', \
-			'" + $('strBlockComment') + "') \
-			ON DUPLICATE KEY UPDATE \
-			`Hopper`= '" + $('strHopper') + "', \
-			`WorkstationID`= '" + ('strWorkstationID') + "', \
-			`SpecNumFormatted`= '" + $('strSpecNumFormatted') + "', \
-			`PatientName`= '" + $('strPatientName') + "', \
-			`SpecimenYear`= '" + $('strSpecYear') + "', \
-			`SpecimenNumber`= '" + $('strSpecNum')+ "', \
-			`Template`= '" + $('strTemplate') + "', \
-			`Specimen_id`= '" + strSpecimenID + "', \
-			`DateTimeEngraved` = '" + strDateTime + "', \
-			`PartInst` = '" + $('intPartInst') + "', \
-			`BlockInst` = '" + $('intBlockInst') + "', \
-			`WhoEngraved` = '" + $('strWhoEngraved') + "', \
-			`PartType` = '" + $('strPartType') + "', \
-			`PartDescription` = '" + $('strPartDesc') + "', \
-			`DateOfService` = '" + $('strDateTaken') + "', \
-			`DateReceived` = '" + $('strDateReceived') + "', \
-			`Protocol` = '" + $('strProtocolID') + "', \
-			`Sequence` = '" + $('intBlockSequence') + "', \
-			`PartSequence` = '" + $('intPartSequence') + "', \
-			`PartComment` = '" + $('strPartComment') + "', \
-			`Pieces` = '" + $('intBlockPieces') + "', \
-			`BlockDept` = '" + $('strBlockDept') + "', \
-			`RequestClass` = '" + $('strRequestClass') + "', \
-			`BlockDesignator` = '" + $('strBlockLabel') + "', \
-			`PartDesignator` = '" + $('strSpecLabel') + "', \
-			`BlockComment` = '" + $('strBlockComment')  + "', \
-			`Notes` = \"Cassette printed more than once.  Printed again on: '" + strDateTime + "'.  Initial DateTime Engraved kept, other values have been updated.\";";
+  strSQL = "INSERT INTO `tblBlock` \
+  			(`BlockID`, \
+  			`Hopper`, \
+  			`WorkstationID`, \
+  			`SpecNumFormatted`, \
+  			`PatientName`, \
+  			`SpecimenYear`, \
+  			`SpecimenNumber`, \
+  			`Template`, \
+  			`Specimen_id`, \
+  			`DateTimeEngraved`, \
+  			`PartInst`, \
+  			`BlockInst`, \
+  			`WhoEngraved`, \
+  			`PartType`, \
+  			`PartDescription`, \
+  			`DateOfService`, \
+  			`DateReceived`, \
+  			`Protocol`, \
+  			`Sequence`, \
+  			`PartSequence`, \
+  			`PartComment`, \
+  			`Pieces`, \
+  			`BlockDept`, \
+  			`RequestClass`, \
+  			`BlockDesignator`, \
+  			`PartDesignator`, \
+  			`TimesEngraved`, \
+  			`LastTimeEngraved`, \
+  			`Audit`, \
+  			`BlockComment`) \
+  		VALUES \
+  			('" + $('strBlockID') + "', \
+  			'" + $('strHopper') + "', \
+  			'" + $('strWorkstationID') + "', \
+  			'" + $('strSpecNumFormatted') + "', \
+  			'" + $('strPatientName') + "', \
+  			'" + $('strSpecYear') + "', \
+  			'" + $('strSpecNum') + "', \
+  			'" + $('strTemplate') + "', \
+  			'" + strSpecimenID + "', \
+  			'" + strDateTime + "', \
+  			'" + $('intPartInst') + "', \
+  			'" + $('intBlockInst') + "', \
+  			'" + $('strWhoEngraved') + "', \
+  			'" + $('strPartType') + "', \
+  			'" + $('strPartDesc') + "', \
+  			'" + $('strDateTaken') + "', \
+  			'" + $('strDateReceived') + "', \
+  			'" + $('strProtocolID') + "', \
+  			'" + $('intBlockSequence') + "', \
+  			'" + $('intPartSequence') + "', \
+  			'" + $('strPartComment')  + "', \
+  			'" + $('intBlockPieces') + "', \
+  			'" + $('strBlockDept') + "', \
+  			'" + $('strRequestClass') + "', \
+  			'" + $('strBlockLabel') + "', \
+  			'" + $('strSpecLabel') + "', \
+  			1, \
+  			'" + strDateTime + "', \
+                 '" + strDateTime + " cassette printed by: " + $('strWhoEngraved')+ " at "+ $('strWorkstationID') + " on hopper "+ $('strHopper') +".', \
+  			'" + $('strBlockComment') + "') \
+  			ON DUPLICATE KEY UPDATE \
+  			`Hopper`= '" + $('strHopper') + "', \
+  			`WorkstationID`= '" + ('strWorkstationID') + "', \
+  			`SpecNumFormatted`= '" + $('strSpecNumFormatted') + "', \
+  			`PatientName`= '" + $('strPatientName') + "', \
+  			`SpecimenYear`= '" + $('strSpecYear') + "', \
+  			`SpecimenNumber`= '" + $('strSpecNum')+ "', \
+  			`Template`= '" + $('strTemplate') + "', \
+  			`Specimen_id`= '" + strSpecimenID + "', \
+  			`DateTimeEngraved` = '" + strDateTime + "', \
+  			`PartInst` = '" + $('intPartInst') + "', \
+  			`BlockInst` = '" + $('intBlockInst') + "', \
+  			`WhoEngraved` = '" + $('strWhoEngraved') + "', \
+  			`PartType` = '" + $('strPartType') + "', \
+  			`PartDescription` = '" + $('strPartDesc') + "', \
+  			`DateOfService` = '" + $('strDateTaken') + "', \
+  			`DateReceived` = '" + $('strDateReceived') + "', \
+  			`Protocol` = '" + $('strProtocolID') + "', \
+  			`Sequence` = '" + $('intBlockSequence') + "', \
+  			`PartSequence` = '" + $('intPartSequence') + "', \
+  			`PartComment` = '" + $('strPartComment') + "', \
+  			`Pieces` = '" + $('intBlockPieces') + "', \
+  			`BlockDept` = '" + $('strBlockDept') + "', \
+  			`RequestClass` = '" + $('strRequestClass') + "', \
+  			`BlockDesignator` = '" + $('strBlockLabel') + "', \
+  			`PartDesignator` = '" + $('strSpecLabel') + "', \
+  			`BlockComment` = '" + $('strBlockComment')  + "', \
+  			`TimesEngraved` = `TimesEngraved`+1, \
+  			`LastTimeEngraved` = '" + strDateTime  + "', \
+  			`Audit` = CONCAT(`Audit`, ' #', '" + strDateTime + "', ' ', 'Cassette printed by: " + $('strWhoEngraved')+ " at "+ $('strWorkstationID') + " on hopper "+ $('strHopper') +".'), \
+  			`Notes` = \"Cassette printed more than once.  Printed again on: '" + strDateTime + "'.  Initial DateTime Engraved kept, other values have been updated.\";";
 
 		//logger.debug("Insert Block SQL:" + strSQL);
 		result = dbConnMYSQL.executeUpdate(strSQL);
