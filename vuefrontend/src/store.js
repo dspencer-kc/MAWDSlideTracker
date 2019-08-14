c78836d5aac23f45fe7c3dedc05d3278258cc3c2
Drew <drw.spencer@gmail.com>
2019-08-14 12:40:34 -0500

    Change FrontEnd API to prod

--- vuefrontend/src/store.js
+++ vuefrontend/src/store.js
@@ -12,9 +12,9 @@ export default new Vuex.Store({
     stationName: '',
     slideQueuePath: '',
     //  Prod
-    //  apiURL: 'http://10.24.4.9:2081',
+    apiURL: 'http://10.24.4.9:2081',
     //  Test
-    apiURL: 'http://10.24.4.9:2082',
+    //  apiURL: 'http://10.24.4.9:2082',
     //  Local Test
     // apiURL: 'http://localhost:2081',
     // Note `isActive` is left out and will not appear in the rendered table
