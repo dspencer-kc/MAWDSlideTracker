0265a3148656ae247d07195300cea338e52aea77
Drew <drw.spencer@gmail.com>
2019-08-16 16:10:18 -0500

    InitialRoutes and component set up

--- vuefrontend/src/routes.js
+++ vuefrontend/src/routes.js
@@ -3,11 +3,13 @@
 import Home from './components/SlidesV3.vue'
 // import SlidePrinter from './components/SlidesV3.vue'
 import SlideDistribution from './components/SlideDistribution.vue'
+import PathConsole from './components/PathConsole.vue'
 
 const routes = [
   { path: '/', component: Home, name: 'home', props: true },
   // { path: '/slideprinter', component: SlidePrinter, name: 'SlidePrinter', props: true }
-  { path: '/slidedistribution', component: SlideDistribution, name: 'SlideDistribution', props: true }
+  { path: '/slidedistribution', component: SlideDistribution, name: 'SlideDistribution', props: true },
+  { path: '/pathconsole', component: PathConsole, name: 'PathConsole', props: true }
 ]
 
 export default routes
