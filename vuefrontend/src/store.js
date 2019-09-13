6844c8a8db019849a93289e8ee4e208a7d66b2c6
Drew <drw.spencer@gmail.com>
2019-09-13 12:58:49 -0500

    v3.03 moved to live

--- vuefrontend/src/store.js
+++ vuefrontend/src/store.js
@@ -14,7 +14,7 @@ export default new Vuex.Store({
     //  Prod
     apiURL: 'http://10.24.4.9:2081',
     //  Test
-    apiURL: 'http://10.24.4.9:2082',
+    //  apiURL: 'http://10.24.4.9:2082',
     //  Local Test
     //  apiURL: 'http://localhost:2081',
     // Note `isActive` is left out and will not appear in the rendered table
