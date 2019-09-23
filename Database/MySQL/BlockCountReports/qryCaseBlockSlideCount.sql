5da60ba1e63f91eb7c65641e81024728fec14ff7
Drew <drw.spencer@gmail.com>
2019-09-23 10:36:23 -0500

    Added Query

--- Database/MySQL/BlockCountReports/qryCaseBlockSlideCount.sql
+++ Database/MySQL/BlockCountReports/qryCaseBlockSlideCount.sql
@@ -57,3 +57,33 @@ FROM (SELECT subTblSlides.AccessionID AS subAccessionID, subTblSlideDistribution
          SlideDistributionLocation = 'LOCNDERM'
         GROUP BY subTblSlides.AccessionID, SlideDistributionLocation) as qrySubBlockCountWLocation
 GROUP BY SlideDistributionLocation;
+
+/*qryThirdRunSlideCountDerm*/
+SELECT COUNT(subTblSlides.SlideID) AS ThirdRunSlideCount
+        FROM tblSlides as subTblSlides
+        INNER JOIN   tblSlideDistribution as subTblSlideDistribution on subTblSlides.SlideDistributionID = subTblSlideDistribution.SlideDistributionID
+        WHERE subTblSlideDistribution.DTReadyForCourier >= funCurrentDaySecondRunCutoff()
+         AND  subTblSlideDistribution.DTReadyForCourier < funCurrentDaySecondRunCutoff() AND
+         SlideDistributionLocation = 'LOCNDERM';
+
+/*qrySecondRunBlockCountDerm*/
+SELECT Count(qrySubBlockCountWLocation.subBlockID) AS SecondRunBlockCount
+FROM (SELECT subTblSlides.BlockID AS subBlockID, subTblSlideDistribution.SlideDistributionLocation
+        FROM tblSlides as subTblSlides
+        INNER JOIN   tblSlideDistribution as subTblSlideDistribution on subTblSlides.SlideDistributionID = subTblSlideDistribution.SlideDistributionID
+        WHERE subTblSlideDistribution.DTReadyForCourier >= funCurrentDaySecondRunCutoff()
+         AND  subTblSlideDistribution.DTReadyForCourier < funCurrentDaySecondRunCutoff() AND
+         SlideDistributionLocation = 'LOCNDERM'
+        GROUP BY subTblSlides.BlockID, SlideDistributionLocation) as qrySubBlockCountWLocation
+GROUP BY SlideDistributionLocation;
+
+/*qrySecondRunCaseCountDerm*/
+SELECT Count(qrySubBlockCountWLocation.subAccessionID) AS SecondRunCaseCount
+FROM (SELECT subTblSlides.AccessionID AS subAccessionID, subTblSlideDistribution.SlideDistributionLocation
+        FROM tblSlides as subTblSlides
+        INNER JOIN   tblSlideDistribution as subTblSlideDistribution on subTblSlides.SlideDistributionID = subTblSlideDistribution.SlideDistributionID
+        WHERE subTblSlideDistribution.DTReadyForCourier >= funCurrentDaySecondRunCutoff()
+         AND  subTblSlideDistribution.DTReadyForCourier < funCurrentDaySecondRunCutoff() AND
+         SlideDistributionLocation = 'LOCNDERM'
+        GROUP BY subTblSlides.AccessionID, SlideDistributionLocation) as qrySubBlockCountWLocation
+GROUP BY SlideDistributionLocation;
