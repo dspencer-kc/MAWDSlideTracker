d6791a52e9bab2bf9870cf17e42723c7a5b4fc2c
Justin Dial <justinalandial@gmail.com>
2021-06-21 12:05:25 -0500

    Update SlideDistribution.vue

--- vuefrontend/src/components/SlideDistribution.vue
+++ vuefrontend/src/components/SlideDistribution.vue
@@ -37,6 +37,7 @@
 
   <br>
   <div class="customsubheadertext">
+    <p v-bind:style="getColor(currentslidetray)" >Current Slide Tray: {{currentslidetray}}    Slide Count in Tray: {{strInTraySlideCount}}    Block Count in Tray: {{strInTrayBlockCount}}</p>
     <h5>Slide Details in Current Tray: </h5>
   </div>
 
